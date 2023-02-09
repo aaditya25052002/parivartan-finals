899c4cf0d6104bfc9be8881a33e520d9a9e7993a
dixitt5 <tilajidixit@gmail.com>
2023-02-09 20:55:58 +0530

    Styled the chatbot

--- src/components/Chatbot.js
+++ src/components/Chatbot.js
@@ -1,24 +1,5 @@
 import React from "react";
-import ChatBot from "react-simple-chatbot";
-const steps = [
-  {
-    id: "0",
-    message: "Welcome to react chatbot!",
-    trigger: "1",
-  },
-  {
-    id: "1",
-    message: "Bye!",
-    end: true,
-  },
-];
-const Chatbot = () => {
-  return (
-    <div>
-      <ChatBot steps={steps} />
-      {/* <h1>hii</h1> */}
-    </div>
-  );
-};
 
-export default Chatbot;
+export default function Chatbot() {
+  return <div>Chatbot</div>;
+}
